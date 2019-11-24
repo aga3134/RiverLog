f6191f46704451c92567b1f17b88bfd73aeea238
aga3134 <aga3134@gmail.com>
2019-11-24 17:41:35 +0800

    disable water level gate url encoding

--- static/js/main.js
+++ static/js/main.js
@@ -745,7 +745,7 @@ var g_APP = new Vue({
       arr.push({value: (this.waterLevelOption.showDrain?1:0),bitNum: 1});
       arr.push({value: (this.waterLevelOption.showAgri?1:0),bitNum: 1});
       arr.push({value: (this.waterLevelOption.showSewer?1:0),bitNum: 1});
-      arr.push({value: (this.waterLevelOption.showGate?1:0),bitNum: 1});
+      //arr.push({value: (this.waterLevelOption.showGate?1:0),bitNum: 1});
       arr.push({value: this.waterLevelOption.thresh,bitNum: 8});
 
       //reservoir option
@@ -857,7 +857,7 @@ var g_APP = new Vue({
       bitNumArr.push({name:"waterLevelShowDrain",bitNum:1});
       bitNumArr.push({name:"waterLevelShowAgri",bitNum:1});
       bitNumArr.push({name:"waterLevelShowSewer",bitNum:1});
-      bitNumArr.push({name:"waterLevelShowGate",bitNum:1});
+      //bitNumArr.push({name:"waterLevelShowGate",bitNum:1});
       bitNumArr.push({name:"waterLevelThresh",bitNum:8});
 
       //reservoir option
@@ -924,7 +924,7 @@ var g_APP = new Vue({
       this.waterLevelOption.showDrain = valueArr["waterLevelShowDrain"]==1?true:false;
       this.waterLevelOption.showAgri = valueArr["waterLevelShowAgri"]==1?true:false;
       this.waterLevelOption.showSewer = valueArr["waterLevelShowSewer"]==1?true:false;
-      this.waterLevelOption.showGate = valueArr["waterLevelShowGate"]==1?true:false;
+      //this.waterLevelOption.showGate = valueArr["waterLevelShowGate"]==1?true:false;
       this.waterLevelOption.thresh = valueArr["waterLevelThresh"];
 
       this.reservoirOption.collapse = valueArr["reservoirCollapse"]==1?true:false;
