76a1342374c6a3984ea1df522a50004d69d9d7e2
aga3134 <aga3134@gmail.com>
2019-12-08 14:26:02 +0800

    add data cluster to mapPump

--- static/js/map/mapPump.js
+++ static/js/map/mapPump.js
@@ -22,15 +22,31 @@ class MapPump extends MapLayer{
     UpdateInfoWindow(d){
     	var str = "";
 		var loc = null;
-		var s = this.data.site[d.stationNo];
-	    str = "<p>"+s.name+" 抽水站</p>";
-	    str += "<p>內水水位 "+d.levelIn.toFixed(2)+" m";
-	    str += "<p>外水水位 "+d.levelOut.toFixed(2)+" m";
-	    str += "<p>總抽水狀態: "+d.allPumbLights;
-	    str += "<p>時間 "+d.time+" </p>";
-	    //str += "<div class='info-bt-container'><div class='info-bt' onclick='g_APP.mapControl.OpenLineChart(\"pump\");'>今日變化</div></div>";
-	    loc = new google.maps.LatLng(s.lat, s.lng); 
-		
+		var str = "";
+		var loc = null;
+		if(d.num){	//cluster
+			var lat = d.latSum/d.num;
+			var lng = d.lngSum/d.num;
+			var levelIn = d.levelInSum/d.num;
+			var levelOut = d.levelOutSum/d.num;
+			str = "<p>平均抽水站狀態</p>";
+			str += "<p>抽水站數 "+d.num+"</p>";
+			str += "<p>平均內水水位 "+levelIn.toFixed(2)+" m </p>";
+			str += "<p>平均外水水位 "+levelOut.toFixed(2)+" m </p>";
+			str += "<p>總抽水狀態: "+d.allPumbLights;
+			str += "<p>時間 "+d.t+" </p>";
+			loc = new google.maps.LatLng(lat,lng);
+		}
+		else{
+			var s = this.data.site[d.stationNo];
+		    str = "<p>"+s.name+" 抽水站</p>";
+		    str += "<p>內水水位 "+d.levelIn.toFixed(2)+" m";
+		    str += "<p>外水水位 "+d.levelOut.toFixed(2)+" m";
+		    str += "<p>總抽水狀態: "+d.allPumbLights;
+		    str += "<p>時間 "+d.time+" </p>";
+		    //str += "<div class='info-bt-container'><div class='info-bt' onclick='g_APP.mapControl.OpenLineChart(\"pump\");'>今日變化</div></div>";
+		    loc = new google.maps.LatLng(s.lat, s.lng); 
+		}
 		this.infoWindow.setOptions({content: str, position: loc});
     }
 
@@ -155,22 +171,82 @@ class MapPump extends MapLayer{
 
 		var baseSize = this.GetBaseScale()*g_APP.waterLevelOption.scale;
 		var bound = this.map.getBounds();
-		for(var i=0;i<pumpData.length;i++){
-			var sID = pumpData[i].stationNo;
-			var s = this.data.site[sID];
-			if(!s) continue;
-			if(!bound.contains({lat:s.lat,lng:s.lng})) continue;
-
-			var d = pumpData[i];
-			var clickFn = this.GenClickFn(pumpData,i,"stationNo");
-
-			//info window有打開，更新資訊
-			if(this.infoWindow.getMap() && this.infoTarget == sID){
-				this.UpdateInfoWindow(pumpData[i]);
+		var zoom = this.map.getZoom();
+		var isCluster = (zoom<=10);
+		if(isCluster){
+			var step = 0.04*Math.min(4,Math.pow(2,10-zoom));
+			var clusterHash = {};
+			for(var i=0;i<pumpData.length;i++){
+				var data = pumpData[i];
+				var sID = data.stationNo;
+				var s = this.data.site[sID];
+				if(!s) continue;
+				if(!bound.contains({lat:s.lat,lng:s.lng})) continue;
+				var x = Math.round(s.lat/step);
+				var y = Math.round(s.lng/step);
+				var key = x+"-"+y;
+				if(!clusterHash[key]){
+					var acc = {};
+					acc.key = key;
+					acc.t = data.time;
+					acc.levelInSum = data.levelIn;
+					acc.levelOutSum = data.levelOut;
+					acc.allPumbLights = data.allPumbLights;
+					acc.latSum = s.lat;
+					acc.lngSum = s.lng;
+					acc.num = 1;
+					clusterHash[key] = acc;
+				}
+				else{
+					clusterHash[key].levelInSum += data.levelIn;
+					clusterHash[key].levelOutSum += data.levelOut;
+					if(clusterHash[key].allPumbLights == "-"){
+						clusterHash[key].allPumbLights = data.allPumbLights;
+					}
+					clusterHash[key].latSum += s.lat;
+					clusterHash[key].lngSum += s.lng;
+					clusterHash[key].num += 1;
+				}
 			}
+			var cluster = [];
+			for(var key in clusterHash){
+				cluster.push(clusterHash[key]);
+			}
+			for(var i=0;i<cluster.length;i++){
+				var d = cluster[i];
+				var key = d.key;
+				var lat = d.latSum/d.num;
+				var lng = d.lngSum/d.num;
+				var levelIn = d.levelInSum/d.num;
+				var levelOut = d.levelOutSum/d.num;
+				var pumping = d.allPumbLights=="-"?false:true;
+				var clickFn = this.GenClickFn(cluster,i,"key");
 
-			var pumping = d.allPumbLights=="-"?false:true;
-			this.UpdateIcon(sID,s.lat,s.lng,d.levelIn,d.levelOut,pumping,clickFn);
+				//info window有打開，更新資訊
+				if(this.infoWindow.getMap() && this.infoTarget == key){
+					this.UpdateInfoWindow(cluster[i]);
+				}
+				this.UpdateIcon(key,lat,lng,levelIn,levelOut,pumping,clickFn);
+			}
+		}
+		else{
+			for(var i=0;i<pumpData.length;i++){
+				var sID = pumpData[i].stationNo;
+				var s = this.data.site[sID];
+				if(!s) continue;
+				if(!bound.contains({lat:s.lat,lng:s.lng})) continue;
+
+				var d = pumpData[i];
+				var clickFn = this.GenClickFn(pumpData,i,"stationNo");
+
+				//info window有打開，更新資訊
+				if(this.infoWindow.getMap() && this.infoTarget == sID){
+					this.UpdateInfoWindow(pumpData[i]);
+				}
+
+				var pumping = d.allPumbLights=="-"?false:true;
+				this.UpdateIcon(sID,s.lat,s.lng,d.levelIn,d.levelOut,pumping,clickFn);
+			}
 		}
 
 	}
